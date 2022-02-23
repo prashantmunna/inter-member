3fef9d34f5e8eaee39cb36b516aa0287e806f8b3
Prashant munna <pkm98@outlook.com>
2022-02-23 10:38:39 +0530

    Edit name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { takeUntil } from 'rxjs/operators';
 import { ApiService } from './api.service';
 
@@ -11,6 +12,15 @@ export class AppComponent {
   title = 'interview-test';
   member_list :any=[]
   search_list:any=[]
+  hideList:boolean=true;
+  hideEdit:boolean=false;
+  showUpdatedName:boolean=false;
+  updatedName;
+  // editName:FormGroup
+  editName = new FormGroup({
+    name: new FormControl('')
+
+  })
   constructor(private  api:ApiService) { }
   ngOnInit(): void {
     this.api.getList().pipe().subscribe(data => {
@@ -30,8 +40,22 @@ export class AppComponent {
   }
   edit(name){
     console.log(name)
+    this.hideList=false;
+    this.hideEdit=true;
+    this.editName.controls.name.patchValue(name);
 
 
+  }
+  save(){
+    this.updatedName=this.editName.value.name;
+    console.log(this.updatedName)
+    this.hideEdit=false;
+    this.showUpdatedName=true;
+
+  }
+  List(){
+    this.hideList=true;
+    this.showUpdatedName=false;
   }
 
   
